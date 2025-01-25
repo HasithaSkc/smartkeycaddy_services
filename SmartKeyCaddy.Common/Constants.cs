e70f7aaa4e3f42242bfeaf2e9a773dee21a45306
HasithaAma <hasithapg@gmail.com>
2025-01-25 11:45:01 +1100

    Corrected the ISO time

--- SmartKeyCaddy.Common/Constants.cs
+++ SmartKeyCaddy.Common/Constants.cs
@@ -4,7 +4,7 @@
     {
         public const string SmartKeyCaddySchemaName = "smartkeycaddyuser";
         public const string ShortDateString = "yyyy-MM-dd";
-        public const string IsoDateString = "yyyy-MM-ddTHH:mm:ss.fffffffK";
+        public const string IsoDateString = "yyyy-MM-ddTHH:mm:ss.fffffff";
         public const string KeyAllocationRequestHandler = "KeyAllocationRequestHandler";
         public const string ForceBinOpenRequestHandler = "ForceBinOpenRequestHandler";
         public const string KeyDeleteRequestHandler = "KeyDeleteRequestHandler";
